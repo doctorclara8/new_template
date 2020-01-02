25c0039404cab17ed8fe30f779e5ee026b0b4398
doctorclara8 <alionaotp2015@gmail.com>
2020-01-02 11:24:21 +0200

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,13 @@
+$(function () {
+	$('#my-menu').mmenu({
+		extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black', "border-none", "position-back",
+			"position-left"]
+	});
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:finish', function () {
+		$('.hamburger').addClass('is-active');
+	}).bind('close:finish', function () {
+		$('.hamburger').removeClass('is-active');
+	});
+
+});
